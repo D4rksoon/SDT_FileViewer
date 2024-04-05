ee73b1e1574bae487c4521efec9d074e71f6f060
D4rksoon <Asdreem2539@gmail.com>
2024-04-05 14:34:58 +0800

    Описание заголовочного файла FileManager.h
    Создан подкласс File

---
+++ FileViewer/filemanager.h
@@ -0,0 +1,26 @@
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include <QFile>
+#include <QFileInfo>
+#include <QString>
+#include <vector>
+
+class File : public QFile
+{
+public:
+    File(const QString &name) : QFile(name){}
+    ~File() = default;
+};
+
+class FileManager
+{
+public:
+    FileManager(int size = 0);
+    File* addFile(QString &name);
+
+private:
+    std::vector<File*> vecFiles;
+};
+
+#endif // FILEMANAGER_H
